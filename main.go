b994c5006b812ea5a9f3d1a3cea1fe4a2fa00a88
Saif Rizvi <saifberuf1@outlook.de>
2025-11-10 14:51:35 +0100

    testing for loop

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
-	fmt.Println("hello world")
-	a, b, c := 80, 80, 80
-	fmt.Println(a, b, c)
-	d := a + b
+
+	a := 10 // inferred type int
+	b := 3.14 // inferred type float64
+	//fmt.Println(a, b)
+	
+	c := float64(a)
+
+	fmt.Println(c + b)
+	
+	text := "I AM A STRING ... "
+	text2 := "I AM ANOTHER STRING"
+	fmt.Println(text + text2)
+	
+	d := math.MaxFloat64
 	fmt.Println(d)
+
+	g := 6
+	h := 7 
+
+	if (g > h) {
+		fmt.Println("76")
+	} else if (h > g + 2) {
+		fmt.Println("67")
+	} else if (h > g - 2) {
+		fmt.Println("OK")
+	} else {
+		fmt.Println("error")
+	}
+
+
+	    // for: loop
+    for i := 0; i < 100; i++ {
+        fmt.Println("Loop iteration:", i)
+    }
 }
